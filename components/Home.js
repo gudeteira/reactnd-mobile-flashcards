7af35e3b2030461accf7f5695e47392b02a06bcc
pedro.gude <gudeteira@gmail.com>
2019-05-12 05:48:09 +0200

    feat: Add toolbar

--- components/Home.js
+++ components/Home.js
@@ -1,24 +1,42 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
-import {FAB} from 'react-native-paper';
+import {StyleSheet, View} from 'react-native';
 import {connect} from 'react-redux';
-import {Colors, Theme} from '../theme/theme';
+import {Routes} from '../router/Routes';
 import DeckList from './DeckList';
+import IconButton from './IconButton';
+import Toolbar from './Toolbar';
 
 class Home extends Component {
 
   addDeck = () => {
-    this.props.navigation.navigate('AddDeck')
+    this.props.navigation.navigate(Routes.AddDeck);
   };
 
   render() {
     return (
-      <View style={Theme.homeContainer}>
+      <View style={{flex: 1}}>
+        <Toolbar title='My Flashcards'/>
         <DeckList/>
-        <FAB style={Theme.fab} icon="add" color={Colors.text} onPress={this.addDeck}/>
+        <IconButton style={styles.icon} name="add" size={30} color="white" onPress={this.addDeck}/>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+
+  icon: {
+    borderWidth: 1,
+    borderColor: 'red',
+    width: 55,
+    height: 55,
+    backgroundColor: 'red',
+    borderRadius: 50,
+    position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 0,
+  },
+});
 
 export default connect()(Home);
