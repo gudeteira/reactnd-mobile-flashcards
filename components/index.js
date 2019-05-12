b8d099f886c17b401dbddfdf5b8161c1ba5b52d0
pedro.gude <gudeteira@gmail.com>
2019-05-12 05:48:49 +0200

    feat: Export new components

--- components/index.js
+++ components/index.js
@@ -3,3 +3,5 @@ export Home from './Home';
 export Deck from './Deck';
 export AddDeck from './AddDeck';
 export AddQuestion from './AddQuestion';
+export Question from './Question';
+export QuizSummary from './QuizSummary';
