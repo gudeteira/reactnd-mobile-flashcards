a15350503397351673603829925896a8ccbde449
pedro.gude <gudeteira@gmail.com>
2019-05-12 17:02:11 +0200

    feat: Randomize background color

--- components/card/Avatar.js
+++ components/card/Avatar.js
@@ -5,13 +5,19 @@ class Avatar extends PureComponent {
   render() {
     const {text} = this.props;
     return (
-      <View style={styles.container}>
-        <Text style={{color: 'white',}}>{text}</Text>
+      <View style={[styles.container, {backgroundColor: this.backGroundColor()}]}>
+        <Text style={{color: 'white',}}>{text ? text.toUpperCase() : text}</Text>
       </View>
     );
   }
+
+  backGroundColor = () => {
+    const index = Math.floor((Math.random() * bgColors.length - 1) + 1);
+    return bgColors[index];
+  };
 }
 
+const bgColors = ['#ccabd8', '#d0e6a5', '#ffdd94', '#fa897b', '#ccabd8', '#FEA735', '#00C3FF',];
 const styles = StyleSheet.create({
   container: {
     width: 48,
@@ -20,7 +26,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     color: 'white',
-    backgroundColor: '#ccabd8'// d0e6a5, ffdd94, fa897b, ccabd8
+    backgroundColor: '#ccabd8'
   },
 });
 
